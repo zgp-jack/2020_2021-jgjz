99faacd8164892cf55b17a2a91aecd24d1194259
zyb140015 <568272239@qq.com>
2020-07-24 14:28:03 +0800

    1

--- Bookkeeping/src/pages/index/index.tsx
+++ Bookkeeping/src/pages/index/index.tsx
@@ -141,16 +141,18 @@ export default function Index() {
           if(res.code == 200){
             console.log(res.data,'res.data');
             console.log(321321)
-            let obj:any={};
-            obj = res.data;
-            obj.userId = e.referrerInfo.extraData.userId;
-            obj.token = e.referrerInfo.extraData.token;
-            obj.tokenTime = e.referrerInfo.extraData.tokenTime;
-            obj.sign.token = e.referrerInfo.extraData.token;
-            obj.sign.time = e.referrerInfo.extraData.tokenTime;
-            console.log(obj,'redadmskldnmaslkdnlkasn')
-            Taro.setStorageSync(MidData, obj);
-            getData();
+            if(res.data){
+              let obj:any={};
+              obj = res.data;
+              obj.userId = e.referrerInfo.extraData.userId;
+              obj.token = e.referrerInfo.extraData.token;
+              obj.tokenTime = e.referrerInfo.extraData.tokenTime;
+              obj.sign.token = e.referrerInfo.extraData.token;
+              obj.sign.time = e.referrerInfo.extraData.tokenTime;
+              console.log(obj,'redadmskldnmaslkdnlkasn')
+              Taro.setStorageSync(MidData, obj);
+              getData();
+            }
             // 没有鱼泡账号
           } else if (res.code == 40001){
             //  有鱼泡账号
