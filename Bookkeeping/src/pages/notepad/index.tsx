1a880bf76bae6afefac32ec758ec19b6ea8306a3
18281686701 <jackzgp@aliyun.com>
2020-08-05 14:27:41 +0800

    GZ-143 【记工记账-记事本列表页】界面文案功能问题

--- Bookkeeping/src/pages/notepad/index.tsx
+++ Bookkeeping/src/pages/notepad/index.tsx
@@ -188,9 +188,11 @@ export default function Notepad() {
       return
     }
     Taro.showModal({
-      title:'提示',
+      // title:'提示',
       content:'删除后，当前信息将无法恢复，确定删除？',
       showCancel: true,
+      confirmColor:'#0099FF',
+      cancelColor:'#797979',
       success: (res) => {
         if (res.confirm == true) {
           // if(ids.length === 0){
