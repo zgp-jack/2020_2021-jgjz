3449885714563cfb162254ef9552f8120b8711e5
西南以北 <568272239@qq.com>
2020-08-07 20:27:56 +0800

    1

--- Bookkeeping/src/pages/index/index.tsx
+++ Bookkeeping/src/pages/index/index.tsx
@@ -173,12 +173,12 @@ export default function Index() {
     }
     return num;
   }
-  // useEffect(()=>{
-  //   if (ContentItem){
-  //     console.log(111);
-  //     setItem(ContentItem)
-  //   }
-  // }, [ContentItem])
+  useEffect(()=>{
+    if (ContentItem){
+      console.log(111);
+      setItem(ContentItem)
+    }
+  }, [ContentItem])
   useEffect(() => {
     Taro.onNetworkStatusChange(function (res) {
       console.log(res.isConnected, 'isConnected')
