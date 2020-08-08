5021518c7257587cdde4568ba79735b17ccfe3eb
西南以北 <568272239@qq.com>
2020-08-08 17:44:34 +0800

    1

--- Bookkeeping/src/components/auth/index.tsx
+++ Bookkeeping/src/components/auth/index.tsx
@@ -286,6 +286,7 @@ export default function Auth({ display, handleClose, callback, loginPhone}: PROP
       userRouteJump(`/pages/login/index?session_key=${item.session_key}&encryptedData=${item.encryptedData}&iv=${item.iv}`)
     }
   }
+  console.log(warrant,'warrant啊啊啊是谁')
   return(
     <View>
       { display && 
