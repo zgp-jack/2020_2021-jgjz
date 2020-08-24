4123b4ba0b231412667309b427c229818917fdfa
西南以北 <568272239@qq.com>
2020-08-24 16:55:48 +0800

    1

--- Bookkeeping/src/pages/index/index.tsx
+++ Bookkeeping/src/pages/index/index.tsx
@@ -951,17 +951,17 @@ export default function Index() {
         return;
       }
       console.log(type,'tyep')
-      if(!type){
+      if(!type || type == 0){
         console.log(312321312);
         setIdentity(true);
-        // return;
+        return;
       }
       // 判断不是0 然后与当前身份不同就是提示
       // 判断后台传过来的状态，然后和这一次的不一样就是有新项目需要出现弹框
       if (parseInt(lasted_business_identity) !== 0 && type != parseInt(lasted_business_identity) && !neverPrompt) {
-        if(type && type !== 0 ){
+        // if(type && type !== 0 ){
           setTips(true)
-        }
+        // }
         return;
       } else {
         if (type === 1) {
