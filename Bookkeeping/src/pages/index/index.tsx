33d95854298558d644a91aba0018d4330829900d
liushaotian <13700942760@163.com>
2020-08-24 20:10:23 +0800

    修改首页展示

--- Bookkeeping/src/pages/index/index.tsx
+++ Bookkeeping/src/pages/index/index.tsx
@@ -1250,7 +1250,7 @@ export default function Index() {
                       <View className='content-list-flex'>
                         <View>{v.workername}{(v.note || v.view_images.length>0)&& <Text className='icon'>备</Text>}</View>
                       </View>
-                      <View className='details'>我在{v.group_info}项目组对{v.workername}记了-笔{v.business_type == '1' ? '记工' : (v.business_type == '2' ? '包工' : '借支')}</View>
+                      <View className='details'>我在{v.group_info}对{v.workername}记了一笔{v.business_type == '1' ? '记工' : (v.business_type == '2' ? '包工' : '借支')}</View>
                     </View>
                     <View className={v.business_type==3?'orgion borrow-blue':'orgion'}><Text className='orgion-type-chars'>¥</Text>{v.money && (parseFloat(v.money) > 9999999.99) ? String(v.money).slice(0, 7) + '...' : v.money || '0.00'}</View>
                     {/* // ))} */}
@@ -1279,7 +1279,7 @@ export default function Index() {
                 <View key={i + i} className='content-list-type' onClick={getNextPageData}>
                   <View className='content-list-flex'>
                     <View className='no-title'>
-                      <Text className='details'>我在{v.group_info}项目组对Ta记了-笔{v.business_type == '1' ? '记工' : (v.business_type == '2' ? '包工' : '借支')}</Text>
+                      <Text className='details'>我在{v.group_info}对Ta记了一笔{v.business_type == '1' ? '记工' : (v.business_type == '2' ? '包工' : '借支')}</Text>
                       {(v.note || v.view_images.length>0)&& <Text className='icon'>备</Text>}
                     </View>
                     <View className={v.business_type==3?'orgion-type borrow-blue':'orgion-type'}><Text className='orgion-type-chars'>¥</Text>{v.money && (parseFloat(v.money) > 9999999.99) ? String(v.money).slice(0, 7) + '...' : v.money || '0.00'}</View>
