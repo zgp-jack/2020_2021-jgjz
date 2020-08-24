0a2265734d1bb41bbfdfc1eedac0abf9ae4550a8
西南以北 <568272239@qq.com>
2020-08-24 17:24:45 +0800

    1

--- Bookkeeping/src/pages/index/index.tsx
+++ Bookkeeping/src/pages/index/index.tsx
@@ -945,7 +945,7 @@ export default function Index() {
       setDisplay(true)
       return;
     }
-    if(midData && type){
+    if(midData && (type!=0 && !type)){
       console.log('aaa')
       setIdentity(true);
       console.log('走这了嗷嗷')
@@ -966,10 +966,10 @@ export default function Index() {
       // 判断不是0 然后与当前身份不同就是提示
       // 判断后台传过来的状态，然后和这一次的不一样就是有新项目需要出现弹框
       if (parseInt(lasted_business_identity) !== 0 && type != parseInt(lasted_business_identity) && !neverPrompt) {
-        // if(type && type !== 0 ){
+        if(type && type !== 0){
           console.log('来了');
           setTips(true)
-        // }
+        }
         return;
       } else {
         if (type === 1) {
